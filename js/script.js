ee78af55ef80298721b99742fd79a27a88e17785
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-02-05 20:55:14 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ documentHeight()
 
 //smooth scroll <=>
 window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+   document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
 })
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
+   wrapper: '.scroll-wrapper',
+   content: '.scroll-content'
 })
